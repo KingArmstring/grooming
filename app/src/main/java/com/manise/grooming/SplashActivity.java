daac696a04995ab3c2d31e0d0e5d91cea7eaffee
KingArmstring <m.elsayed.hamouda@gmail.com>
2021-01-23 16:54:26 +0400

    Adding comments

--- app/src/main/java/com/manise/grooming/SplashActivity.java
+++ app/src/main/java/com/manise/grooming/SplashActivity.java
@@ -13,21 +13,33 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //hiding the toolBar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
         setContentView(R.layout.activity_splash);
+
+
+        //creating an instance of the particleView I integrated through build.gradle
         ParticleView pv = findViewById(R.id.pv_id);
+
+        //adding animation listener to particlesView, this view is being customized in the xml layout file, setting texts, animation duration, ...etc
         pv.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
             @Override
             public void onAnimationEnd() {
+                //upon animation-end, we move to the HomeActivity where the real work happens should happen.
                 startActivity(new Intent(
                     SplashActivity.this,
                     HomeActivity.class
                 ));
+                //finishing this activity
                 finish();
             }
         });
+
+        //starting animation
         pv.startAnim();
     }
 }
