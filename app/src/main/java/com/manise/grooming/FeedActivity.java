1c53c8944860ed92669e6bf5527dd5a3bbc5774c
KingArmstring <m.elsayed.hamouda@gmail.com>
2021-02-13 22:41:44 +0200

    Adding comments

--- app/src/main/java/com/manise/grooming/FeedActivity.java
+++ app/src/main/java/com/manise/grooming/FeedActivity.java
@@ -17,21 +17,37 @@ public class FeedActivity extends AppCompatActivity {
     private List<Post> posts = new ArrayList<>();
     private PostsAdapter adapter;
 
+
+    /**
+     * onCreate is the first method to be executed in the Activity.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //connecting the view(xml) to the java code.
         setContentView(R.layout.activity_feed);
+
+        //referencing the recyclerView widget.
         rvPosts = findViewById(R.id.rv_posts);
+        //calling the methods to setup the list and fill the data
         fillList();
         setupList();
     }
 
+    /**
+     * Setting up the recyclerView which is the list in Android.
+     */
     private void setupList() {
         adapter = new PostsAdapter(posts, this);
         rvPosts.setLayoutManager(new LinearLayoutManager(this));
         rvPosts.setAdapter(adapter);
     }
 
+    /**
+     * Dummy data to test the list
+     */
     private void fillList() {
         posts.add(new Post("This is placeholder text for the post content", R.drawable.ic_doggy));
         posts.add(new Post("This is placeholder text for the post content", R.drawable.ic_doggy));
--- app/src/main/java/com/manise/grooming/PostsAdapter.java
+++ app/src/main/java/com/manise/grooming/PostsAdapter.java
@@ -21,17 +21,29 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
     private List<Post> posts;
     private Context context;
 
+    /**
+     * Constructor
+     * @param posts
+     * @param context
+     */
     PostsAdapter(List<Post> posts, Context context) {
         this.posts = posts;
         this.context = context;
     }
 
+    /**
+     * Inflating the xml layout.
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @Override
     @NonNull
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.post_list_item, parent, false));
     }
 
+    //binding(connecting) the data to the views of the list.
     @Override
     public void onBindViewHolder(PostsAdapter.MyViewHolder holder, int position) {
         holder.postImage.setImageDrawable(ContextCompat.getDrawable(context, posts.get(position).getImageResource()));
@@ -39,13 +51,16 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
         holder.setButtonClickListeners();
     }
 
+    //returning the count of the item being viewed in the list.
     @Override
     public int getItemCount() {
         return posts.size();
     }
 
+    //ViewHolder of the recyclerView.
     class MyViewHolder extends RecyclerView.ViewHolder {
 
+        //declaring all views of the list item layout xml.
         ImageView postImage;
         TextView postText;
         ImageButton btnSave;
@@ -55,6 +70,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
         public MyViewHolder(View itemView) {
             super(itemView);
 
+            //referencing the views of the list item xml layout (see post_list_item)
             postImage = itemView.findViewById(R.id.post_image);
             postText = itemView.findViewById(R.id.post_text);
             btnLike = itemView.findViewById(R.id.btn_like);
@@ -62,16 +78,25 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
             btnDislike = itemView.findViewById(R.id.btn_dislike);
         }
 
+        /**
+         * Buttons click handlers
+         */
         private void setButtonClickListeners() {
+            //Like button click handler
             btnLike.setOnClickListener(v -> {
+                //showing simple placeholder message to the user when the like button gets clicked
                 Toast.makeText(context, "dislike!!", Toast.LENGTH_SHORT).show();
             });
 
+            //Dislike button click handler
             btnDislike.setOnClickListener(v -> {
+                //showing simple placeholder message to the user when the dislike button gets clicked
                 Toast.makeText(context, "like!!", Toast.LENGTH_SHORT).show();
             });
 
+            //Save button click handler
             btnSave.setOnClickListener(v -> {
+                //showing simple placeholder message to the user when the save button gets clicked
                 Toast.makeText(context, "save!!", Toast.LENGTH_SHORT).show();
             });
         }
